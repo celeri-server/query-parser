b1924bcf6b3a86466303973ae4b4b0646cf13991
James Brumond <james@jbrumond.me>
2019-04-26 13:02:11 -0700

    update http-server dependency; add generics typing for input

--- src/extend-request.ts
+++
@@ -1,9 +0,0 @@
-
-import { Request } from '@celeri/http-server';
-import { ParsedUrlQuery } from 'querystring';
-
-declare module '@celeri/http-server' {
-	interface Request {
-		query?: ParsedUrlQuery
-	}
-}
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 
-import './extend-request';
-
 import { parse as parseUrl } from 'url';
 import { parse as parseQueryString } from 'querystring';
 import { MiddlewareFunction } from '@celeri/middleware-pipeline';
 import { MiddlewareInput } from '@celeri/http-server';
-import { } from '@celeri/router';
 
-export const queryParser = () : MiddlewareFunction<MiddlewareInput> => {
+type InputWithQuery = MiddlewareInput<any, { query?: any; }>;
+
+export const queryParser = () : MiddlewareFunction<InputWithQuery> => {
 	return ({ req }): void => {
-		if (req.query) {
+		if (req.querystring) {
 			req.query = parseQueryString(req.querystring);
 		}
+		
 		else {
 			req.query = parseUrl(req.url, true).query;
 		}
